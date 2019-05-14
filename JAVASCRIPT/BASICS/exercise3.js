ffc8dfc8af9373ba07c1d1d89e6e2f9764a987ac
argho <rajatsngpt7@gmail.com>
2019-05-14 15:08:17 +0530

    objects

--- JAVASCRIPT/BASICS/exercise3.js
+++ JAVASCRIPT/BASICS/exercise3.js
@@ -9,7 +9,16 @@
 
 // Javascript is an Object Oriented Programming Language but however it is not a class-based 
 // programming language.
+// Contrast this with a language like java where you have classes which define objects.
+// In order to create an object what you need to do is create a class first,add member variables.
+// add methods to it and then when you create objects off a particular class,I want this object to be off 
+// a class so the class forms the template and then all these different objects are basically copies off that 
+// template this works in a lot of class based object oriented programming languages.
+// However in the case of javascript,there are no classes it is not a class-based object oriented programming 
+// language so you can create objects without having classes in fact its necessary because its there are no
+// classes in javascript,so javascript objects are essentially free form,they are not bound to a particular class.
 
+//Features of the javascript objects
 
 
 
